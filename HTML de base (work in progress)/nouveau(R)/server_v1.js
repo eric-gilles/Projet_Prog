babb342f8c7cbd02543b959e810424f778136a5b
RomainGallerne <romain.gallerne08@gmail.com>
2022-02-21 14:09:30 +0100

    Update server_v1.js

--- HTML de base (work in progress)/nouveau(R)/server_v1.js
+++ HTML de base (work in progress)/nouveau(R)/server_v1.js
@@ -5,18 +5,22 @@ app.listen(8888);
 
 app.get('/',function(request,response) {
     console.log("Chargement de la page");
+    response.setHeader("Access-Control-Allow-Origin","*");
     response.sendFile('QuiEstCe.html',{root: __dirname});
 });
 
 app.get('/fichier/:fichier',function(request,response) {
     console.log("Chargement du fichier :");
+    response.setHeader("Access-Control-Allow-Origin","*");
     console.log(request.params.fichier);
     response.sendFile(request.params.fichier,{root: __dirname});
 });
 
 app.get('/:json',function(request,response) {
-    console.log("Chargement du fichier JSON");
-    let jsonFile = fs.readFileSync(request.params.json,'utf8');
+    console.log("Chargement du fichier JSON :");
+    response.setHeader("Content-Type","application/json");
+    response.setHeader("Access-Control-Allow-Origin","*");
+    let jsonFile = fs.readFileSync(request.params.json);
     let parsed = JSON.parse(jsonFile);
     console.log(parsed);
     response.end(JSON.stringify(parsed));
