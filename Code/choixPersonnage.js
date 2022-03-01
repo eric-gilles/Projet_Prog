db685081c9ecdd7c86a2dccc2ffbe4c731dd1528
MorganNavel <80525825+MorganNavel@users.noreply.github.com>
2022-03-01 16:13:12 +0100

    Update choixPersonnage.js

--- Code/choixPersonnage.js
+++ Code/choixPersonnage.js
@@ -1,16 +1,12 @@
-const { cp } = require("fs");
-
+// const { cp } = require("fs");
 function randomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-function choixPersonnage(){
-    let nbrePerso = 0;
-    console.log("debut decompression fichier JSON");
-    $.getJSON("http://localhost:8888/test.json", function(data){
-        for (p of data.personnages){
-            nbrePerso+=1;
-        }
-    });
-    return (data.personnages[randomInt(nbrePerso)]);
-} 
+function ChoixPersonnage(json){
+    var nbrePerso = 0;
+    for ( p of json.personnages){  
+        nbrePerso+=1;   
+    }    
+    return randomInt(nbrePerso);
+}
