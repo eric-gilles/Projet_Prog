6cf8eef7ed1108bc733ffb10e6d647e2bf7e36be
eric-gilles <77812690+eric-gilles@users.noreply.github.com>
2022-03-01 16:46:18 +0100

    Update choixPersonnage.js

--- Code/choixPersonnage.js
+++ Code/choixPersonnage.js
@@ -10,3 +10,28 @@ function ChoixPersonnage(json){
     }    
     return randomInt(nbrePerso);
 }
+
+//Test
+const nbEssaisNormal = 3;
+var nbEssais = nbEssaisNormal;
+function TestPerso(var index){
+    $.getJSON("http://localhost:8888/test.json", function(data){
+        let html = "<option>...</option>";
+        for(let d of data.personnages){
+            html += "<option>"+d.nom+"</option>";
+        }
+        $("#essai").append(html);
+    });
+    var select = document.getElementById('essai');
+    console.log(nbEssais);
+    if ((select.selectedIndex == index)&&(nbEssais>0)){
+        alert("Bravo ! Vous avez gagnez ! Vous êtes trop fort");
+    }else if (nbEssais==0){
+        if (confirm("Fin du Jeu ! Perdu ! Vous êtes vraiment nul ! Reset :")){
+            nbEssais = nbEssaisNormal;
+        }
+    }
+    else{
+        nbEssais--;
+    }
+}
