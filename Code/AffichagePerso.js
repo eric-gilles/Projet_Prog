9931c548f02aeec0de9af30932188a2fc03ff0c4
RomainGallerne <romain.gallerne08@gmail.com>
2022-03-01 16:35:57 +0100

    Update AffichagePerso.js

--- Code/AffichagePerso.js
+++ Code/AffichagePerso.js
@@ -1,30 +1,37 @@
 const { cp } = require("fs");
 
-function Affichage(){
+function Affichage(json){
     let nbElement = 0;
-    console.log("debut decompression fichier JSON");
-    $.getJSON("http://localhost:8888/test.json", function(data){
-        let html = "<table>";
-        html += "<tr>";
-        let idImg = 0;
-        for(let d of data.personnages){
-            if(nbElement > 4){
-                html += "</tr>";
-                html += "<tr>";
-                nbElement = 0;
+    if (json != null){
+        $("#Personnages").empty();
+        console.log("debut decompression fichier JSON");
+            console.log(json);
+            let html = "<table>";
+            html += "<tr>";
+            let idImg = 0;
+            console.log("1");
+            for(let d of json.personnages){
+                console.log("2");
+                if(nbElement > 4){
+                    html += "</tr>";
+                    html += "<tr>";
+                    nbElement = 0;
+                }
+                nbElement += 1;
+                let fonctionChangeImage = `changeImage('`+idImg+`','`+d.image+`','https://raw.githubusercontent.com/eric-gilles/Projet_Prog/main/images/back.jpg')`;
+                html += `<td class="caseTableau"><img src='`+d.image+`' class="avatar" id="`+idImg+`"`;
+                html += `onclick="`+fonctionChangeImage+`"></div><p class="nom">`;
+                html += d.nom;
+                html += `</p></td>`;
+                idImg += 1;
             }
-            nbElement += 1;
-            let imageLink = `http://localhost:8888/fichier/`+d.image;
-            let fonctionChangeImage = `changeImage('`+idImg+`','`+imageLink+`','https://raw.githubusercontent.com/eric-gilles/Projet_Prog/main/Animation_rotation/back.jpg')`;
-            html += `<td class="caseTableau"><img src='`+imageLink+`' class="avatar" id="`+idImg+`"`;
-            html += `onclick="`+fonctionChangeImage+`"></div><p class="nom">`;
-            html += d.nom;
-            html += `</p></td>`;
-            idImg += 1;
-        }
-        html += "</table>";
-        $("#Personnages").append(html);
-    });
+            console.log("3");
+            html += "</table>";
+            $("#Personnages").append(html);
+    } 
+    else {
+        console.log("Aucun fichier json actif");
+    }  
 }
 
 
@@ -33,10 +40,10 @@ function changeImage(idImg,oldImageLink,newImageLink){
     document.getElementById(idImg).setAttribute("class","rotated");
     setTimeout(function(){
         document.getElementById(idImg).src = newImageLink;
-        console.log(document.getElementById(idImg).clientHeight);
-        console.log(document.getElementById(idImg).clientWidth);
         document.getElementById(idImg).clientHeight = document.getElementById(idImg).clientWidth;
         document.getElementById(idImg).setAttribute("onclick","changeImage('"+idImg+"','"+newImageLink+"','"+oldImageLink+"')");
-        document.getElementById(idImg).setAttribute("class","avatar");
+        setTimeout(function(){
+            document.getElementById(idImg).setAttribute("class","avatar");
+        }, 1000); 
     }, 1000);
 }
