599913491cb096a324f93a1df6f46001bdecce82
MorganNavel <80525825+MorganNavel@users.noreply.github.com>
2022-03-01 15:41:22 +0100

    Update CréationQuestion.js

--- Code/CréationQuestion.js
+++ Code/CréationQuestion.js
@@ -1,46 +1,5 @@
-var x = 1;
-$(document).ready(function() {
-    
-    var max_questions = 10;
-    var div = $("#Questions");
-    var add_button = $("#addquestion");
-    var nbAttr=0;
-    var x = 1;
-    $.getJSON("http://localhost:8888/test.json", function(data){
-        for(let a in data.personnages[0].attributs){
-            let html1="";
-            html1+="<option id="+a+">"+a+"</option>";
-            nbAttr++;
-            $("#attr0").append(html1);
-        }
-    });
-    $(add_button).click(function(e) {
-        e.preventDefault();
-        console.log(x);
-        $.getJSON("http://localhost:8888/test.json", function(data){
-            var html="";
-            if (x < max_questions) {
-                x++;
-                for(let a in data.personnages[0].attributs){
-                    html+="<option id="+a+">"+a+"</option>";
-                    nbAttr++;
-                }
-            $(div).append('<div><select>\n\t<option>ET</option>\n\t<option>OU</option>\n</select>\n<br>Est-ce que <select id="attr"'+(x-1)+'" onchange="'+selectVal(x-1)+'">'+html+'</select><select id="value"'+(x-1)+'></select>?<a href="#" class="delete">supprimer</a></select></div>'); //add select     
-        } else alert('Vous avez assez de questions là! Ca va oui non mais Oh! Vous vous croyez où ?')
-    });
-}); 
-
-    $(div).on("click", ".delete", function(e) {
-        e.preventDefault();
-        $(this).parent('div').remove();
-        x--;
-    });
-    // $("#valider").on('click',function(){
-    //     traitement(x);
-    // });   
-});
-//ça fonctionne mais par contre ne fonctionne pas si on ajoute des questions (elle n'affiche pas les valeurs de l'attributs selectionné) :'(
-function selectVal(i){ 
+function selectVal(i){
+    console.log("Question "+i)
     var value=[]; 
     var html="";
     var cptValeur=0;
@@ -54,7 +13,7 @@ function selectVal(i){
                     if(value.length==0){ //si le tableau est vide alors je rentre la valeur
                         for(valeur of p.attributs[a]){        
                             value.push(valeur);
-                            html+="<option id='value"+i+"'>"+valeur+"</option>";
+                            html+="<option id='value"+cptValeur+"'>"+valeur+"</option>";
                             cptValeur++;
                         }
                     } 
@@ -66,8 +25,10 @@ function selectVal(i){
                             if(!value.includes(valeur)){ 
                                 // console.log("j'ajoute: "+valeur); 
                                 value.push(valeur);
+                                
                                 html+="<option id='value"+cptValeur+"'>"+valeur+"</option>";
                                 cptValeur++;
+                                
                             }
                         } 
                     } 
@@ -77,6 +38,44 @@ function selectVal(i){
         $("#valeur"+i).append(html);
     });
 }
+$(document).ready(function() {
+    var max_questions = 10;
+    var div = $("#Questions");
+    var add_button = $("#addquestion");
+    var x = 1;
+    $.getJSON("http://localhost:8888/test.json", function(data){
+        for(let a in data.personnages[0].attributs){
+            let html1="";
+            html1+="<option id="+a+">"+a+"</option>";
+            
+            $("#attr0").append(html1);
+        }
+    });
+    $(add_button).click(function(e) {
+        e.preventDefault();
+        console.log(x);
+        $.getJSON("http://localhost:8888/test.json", function(data){
+            var html="";
+            if (x < max_questions) {
+                x++;
+                    for(let a in data.personnages[0].attributs){
+                        html+="<option id='"+a+"'>"+a+"</option>";
+                    }
+                    $(div).append('<div><select>\n\t<option>ET</option>\n\t<option>OU</option>\n</select>\n<br>Est-ce que <select id="attr'+(x-1)+'" onchange="">'+html+'</select><select id="valeur'+(x-1)+'"></select>?<a href=# class=delete>supprimer</a></select></div>'); //add select     
+                    $("#attr"+(x-1)).attr("onchange","selectVal("+(x-1)+")");
+            } else alert('Vous avez assez de questions là! Ca va oui non mais Oh! Vous vous croyez où ?')
+        });
+    });
+
+    $(div).on("click", ".delete", function(e) {
+        e.preventDefault();
+        $(this).parent('div').remove();
+        x--;
+    }); 
+});
+
+//ça fonctionne mais par contre ne fonctionne pas si on ajoute des questions (elle n'affiche pas les valeurs de l'attributs selectionné) :'(
+
 //tous ce qui a ici ne fonctionne pas ;
 // function nbPersonnage(){
     
@@ -117,3 +116,5 @@ function selectVal(i){
 //         $("#reponse").append(reponse); 
 //     } 
 // } 
+
+
