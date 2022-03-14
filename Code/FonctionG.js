c1defa491c615f570f086432012ed3d9a2db9706
MorganNavel <80525825+MorganNavel@users.noreply.github.com>
2022-03-14 14:47:44 +0100

    Update FonctionG.js

--- Code/FonctionG.js
+++ Code/FonctionG.js
@@ -1,4 +1,7 @@
 var nbAttribut=1;
+var nbLignes=1;
+var nbLignesMax=0;
+var nbPersonnage=0;
 function AjoutAttribut(){
     let html='<div id="div'+nbAttribut+'"><input type="text" id="attr'+nbAttribut+'"><input onclick="SupprAttribut('+nbAttribut+')" id="suppr'+nbAttribut+'" class="supprA" type="button" value="-"></br></div>';
     $(".listAttribut").append(html);
@@ -7,8 +10,14 @@ function AjoutAttribut(){
     $("#attr").append(html);
     $("#valeurs").append(html2);
     nbAttribut++;
+    if(nbPersonnage==1){//Nombre de ligne est restreint au nombre d'attribut du premier personnage rentré
+        nbLignesMax=nbLignes;
+    }
 }
 function SupprAttribut(indiceA){
+    if(nbPersonnage==1){ //Nombre de ligne est restreint au nombre d'attribut du premier personnage rentré
+        nbLignesMax=nbLignes;
+    }
     $("#div"+indiceA).remove();
     $("#valeur"+indiceA).parent("div").remove();
     $("#LigneAttr"+indiceA).remove();
@@ -16,6 +25,7 @@ function SupprAttribut(indiceA){
         $("#LigneAttr"+i).empty();
         $("#LigneAttr"+i).attr("id","LigneAttr"+(i-1));
         html="<div id='LigneAttr"+(i-1)+"'class='attributs'>Attribut "+(i-1)+":</div>";
+        console.log(html);
         $("#LigneAttr"+(i-1)).append(html);
         $("#suppr"+i).attr("onclick","SupprAttribut("+(i-1)+")");
         $("#attr"+i).attr("id","attr"+(i-1));
@@ -24,4 +34,10 @@ function SupprAttribut(indiceA){
         $("#valeur"+i).attr("id","valeur"+(i-1));
     }
     nbAttribut--;
+}
+//doit prendre toutes les valeurs et les attributs pour faire un dictionnaire comment dans les fichiers JSON
+function getValues(){ 
+    let dicoPersonnage={"nom":$("#valeurNom").val(),"image":$(nbPersonnage-1).attr("src"),"attributs":null}
+    let dicoAttribut={}
+    for(let i=0;i<nbLignes){}
 }
