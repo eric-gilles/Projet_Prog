42c49f7ec5f5f375606fd7159e72eee8ffbe8724
RomainGallerne <romain.gallerne08@gmail.com>
2022-03-21 15:12:47 +0100

    Déplacement fonctions dans FonctionG.js et archivage import.html

--- Générateur/FonctionG.js
+++ Générateur/FonctionG.js
@@ -41,51 +41,56 @@ function SupprAttribut(indiceA){
 }
 
 //Crée l'objet d'un personnage
-function creationObjetPerso(){
-    var personnage = new Object();
-    personnage.nom = $("valeurNom").val;
-    personnage.image = $(nbPersonnage-1).val;
-    personnage.attributs = new Object();
-    for(let i=0;i<nbAttribut;i++){
-        personnage.attributs[$("attr"+i).val] = $("valeur"+i).split(",");
-    }
-    listePersonnages[$("valeurNom").val] = personnage;
+function creationnObjetPerso(){
+    let erreur = false;
+    if($("valeurNom").val != null && $((nbPersonnage-1).toString()).src != null){
+        var personnage = new Object();
+        personnages.nom = $("valeurNom").val;
+        personnages.image = $((nbPersonnage-1).toString()).src;
+        personnages.attributs = new object();
+        for(let i=0;i<nbAttribut;i++){
+            if(personnages.attributs[$("attr"+i).val] != null &&  $("valeur"+i) != null){
+                personnages.attributs[$("attr"+i).val] = $("valeur"+i).split(",");
+            }
+            else {erreur = true;}
+        }
+    } else {erreur = true;}
+    if (!erreur) {listePersonnages[$("valeurNom").val] = personnage;}
 }
 
 //Convertit les personnages en un objet JSON
 function conversionJSON(){
-    var json;// = new object();
+    var json = new object();
     json.personnages = [];
     let index = 0;
     for(let o of listePersonnages){
-        json.personnages[index] = o;
-        index += 1; 
+       json.personnages[index] = o;
+       index += 1; 
     }
     downloadObjectAsJson(json,"GrillePersonnageUtilisateur");
 }
 
 //fonction d'ajout des images et affichage dans le tableau
-function importIMG(){
-    let input = document.getElementById("file");
-    if (input.files && input.files[0]) {
+$("#file").change(function(){
+    if (this.files && this.files[0]) {
         var fr = new FileReader();
         fr.onload = function (e) {
-        nbElement++;
-        if(nbElement == 5) $("#table").find('tbody').append('<tr></tr>');
-        var x = document.createElement("img");
-        x.setAttribute("src", e.target.result);//lien d'environ 62000 caractères
-        x.setAttribute("class", "avatar");
-        x.setAttribute("id", nbPersonnage);
-        document.getElementById("table").rows[colonne].insertCell(-1).appendChild(x);
-        nbPersonnage++;
-    };
-    fr.readAsDataURL(input.files[0]);
+            nbElement++;
+            if(nbElement == 5) $("#table").find('tbody').append('<tr></tr>');
+            var x = document.createElement("img");
+            x.setAttribute("src", e.target.result);//lien d'environ 62000 caractères
+            x.setAttribute("class", "avatar");
+            x.setAttribute("id", idImg);
+            document.getElementById("table").rows[colonne].insertCell(-1).appendChild(x);
+            idImg++;
+        };
+        fr.readAsDataURL(this.files[0]);
         if (nbElement == 5) {
             colonne++;
             nbElement = 0;
         }
     }
-}
+});
 
 //Téléchargement d'un fichier json
 function downloadObjectAsJson(Obj, Name){
